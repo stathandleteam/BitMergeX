12ea2a4d0a34263e9f90aeec57b0a2444d58445b
Adetola Bewaji <adetola013@gmail.com>
2024-11-16 15:18:26 +0100

    lazyloader functionality

--- web-x/src/App.tsx
+++ web-x/src/App.tsx
@@ -3,13 +3,18 @@ import { RouterProvider, useRouter } from './routing/RouterContext';
 import { ROUTES } from './routing/constants';
 import Home from './pages/Home/Home';
 import SignUp from './pages/SignUp/SignUp'
-import Login from './pages/Login/Login'
+// import Login from './pages/Login/Login'
 import SeedPhraseCreate from './pages/SeedPhrase/SeedPhraseCreate';
 import SeedPhraseRecover from './pages/SeedPhrase/SeedPhraseRecover';
 import SeedPhraseConfirm from './pages/SeedPhrase/SeedPhraseConfirm';
 import { AppUX } from './Misc';
 import Password from './pages/Password/Password';
 import WalletCreated from './pages/WalletCreated/WalletCreated';
+import BottomNavigation from './pages/loggedin/Dashboard/BottomNavigation';
+import Dashboard from './pages/loggedin/Dashboard/Dashboard';
+import AccountList from './pages/loggedin/AccountList/AccountList';
+import LazyLoader from './design-system/_components/LazyLoader/LazyLoader';
+
 
 console.log = () => { };
 console.info = () => { };
@@ -21,10 +26,13 @@ console.debug = () => { };
 function Router() {
   const { currentRoute, params } = useRouter();
   const routes:any = {
-    [ROUTES.HOME]: <Home />,
+    [ROUTES.HOME]:  <Home />,
     [ROUTES.AppUX]: <AppUX />,
     [ROUTES.SIGNUP]: <SignUp />,
-    [ROUTES.LOGIN]: <Login />,
+    [ROUTES.LOGIN]: <LazyLoader 
+              importFunc={() => import('./pages/Login/Login')}
+              fallback={<div>Loading...</div>}
+            />,//<Login />,
     [ROUTES.SETTINGS]: <Home />,
     [ROUTES.SEED_PHRASE_CREATE]: <SeedPhraseCreate />,
     [ROUTES.SEED_PHRASE_RECOVER]: <SeedPhraseRecover />,
@@ -32,6 +40,8 @@ function Router() {
     [ROUTES.PASSWORD_SECURITY]: <Password />,
     [ROUTES.ONBOARDING]: <Home />,
     [ROUTES.WALLET_CREATED]: <WalletCreated />,
+    [ROUTES.DASHBOARD]: <Dashboard />,
+    [ROUTES.ACCOUNT_LIST]: <AccountList />
   };
   return routes[currentRoute] || <div>404 Not Found</div>;
 }
